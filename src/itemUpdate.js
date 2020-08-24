6e78d393dee095fc56d163f7d370d4152c5735f7
Edwin Joassart <edwin@hlo.io>
2020-08-25 00:59:58 +0200

    Fix: consider legacy code as reference over spec Enforce strictier definition of Sulfuras and Backstage Passes

--- src/itemUpdate.js
+++ src/itemUpdate.js
@@ -12,9 +12,15 @@ export default class ItemUpdate {
    * @returns {Item} - Updated item
    */
   static update(item) {
-    if (item.name.includes('Sulfuras')) return SulfurasItem.update(item)
-    if (item.name.includes('Backstage passes')) return BackstagePassesItem.update(item)
-    if (item.name.includes('Aged Brie')) return AgedBrieItem.update(item)
+    // Refactoring Note: Legacy code was more strict than specs for
+    // _Sulfuras_ and _Backstage passes_ with only one item accepted for each
+    // category.
+    // An easy way to make it more robust would be to rewrite is as `item.name.includes('Sulfuras')`
+    // In doubt we'll keep behaviour as close as the legacy code as possible
+
+    if (item.name === 'Sulfuras, Hand of Ragnaros') return SulfurasItem.update(item)
+    if (item.name === 'Backstage passes to a TAFKAL80ETC concert') return BackstagePassesItem.update(item)
+    if (item.name === 'Aged Brie') return AgedBrieItem.update(item)
     if (item.name.includes('Conjured')) return ConjuredItem.update(item)
     // add new item type here
     return RegularItem.update(item)
